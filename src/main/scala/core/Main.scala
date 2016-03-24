d849dae34d2ec0cf28f8560accf76889de872300
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2016-03-24 06:57:05 +0100

    Change listening from localhost to 0.0.0.0

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -4,8 +4,8 @@ import enpassant.Supervisor
 
 import akka.actor.{ActorSystem, Props}
 
-case class Config(host: String = "localhost", port: Int = 9100,
-    serviceHost: String = "localhost", servicePort: Int = 9101,
+case class Config(host: String = "0.0.0.0", port: Int = 9100,
+    serviceHost: String = "0.0.0.0", servicePort: Int = 9101,
     router: Option[String] = None, mode: Option[String] = None,
     name: String = "api")
 
@@ -15,11 +15,11 @@ object Main extends App {
     val parser = new scopt.OptionParser[Config]("psmith") {
         head("psmith", "1.0")
         opt[String]('h', "host") action { (x, c) =>
-            c.copy(host = x) } text("host. Default: localhost")
+            c.copy(host = x) } text("host. Default: 0.0.0.0")
         opt[Int]('p', "port") action { (x, c) =>
             c.copy(port = x) } text("port number. Default: 9100")
         opt[String]('H', "service-host") action { (x, c) =>
-            c.copy(serviceHost = x) } text("host name or address. Default: localhost")
+            c.copy(serviceHost = x) } text("host name or address. Default: 0.0.0.0")
         opt[Int]('P', "service-port") action { (x, c) =>
             c.copy(servicePort = x) } text("port number. Default: 9101")
         opt[String]('r', "router") action { (x, c) =>
